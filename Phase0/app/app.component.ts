c627b4680538527016631a4b7fd2f190a8bba8ef
Kevin Dsouza <KevEsha@Kevins-MacBook-Pro.local>
2016-05-31 21:04:53 -0500

    Added contact form component

--- Phase0/app/app.component.ts
+++ Phase0/app/app.component.ts
@@ -1,7 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder , Validators, ControlGroup,AbstractControl} from '@angular/common';
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app/contact-form.component.html'
 })
-export class AppComponent { }
+export class AppComponent { 
+  form: ControlGroup;
+  email: AbstractControl;
+  constructor(private _fb : FormBuilder){
+     this.form = this._fb.group ({
+      
+      'email' : ['', Validators.required]
+      
+    });
+    
+  }
+  
+  ngOnInit() {
+    
+   
+    
+  }
+  
+  
+  
+}
